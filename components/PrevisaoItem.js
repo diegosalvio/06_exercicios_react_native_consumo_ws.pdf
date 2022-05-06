bcb5d7360d984a1cb1eb8422570bf8a8ba640037
Diego Salvio <diantonio46@gmail.com>
2022-05-06 18:04:35 -0300

    Started PrevisaoItem component

--- components/PrevisaoItem.js
+++ components/PrevisaoItem.js
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 const PrevisaoItem = () => {
+
   return (
-    <View>
-      <Text>PrevisaoItem</Text>
-    </View>
+    <Cartao
+        estilos={styles.cartao}>
+        <View 
+            style={styles.tela}>
+            <Image style={styles.tela}  source={{uri: ''}}/>
+        </View>
+
+    </Cartao>
   )
 }
 
 export default PrevisaoItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    cartao: {
+        marginBottom: 8
+    },
+    tela: {
+        flexDirection: 'row'
+    },
+    imagem: {
+        width: 50,
+        height: 50
+    }
+})
