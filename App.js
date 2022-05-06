27de47a455eb297d172995f0534c7aa69c4f74ba
Diego Salvio <diantonio46@gmail.com>
2022-05-06 17:06:04 -0300

    Added .env and states to the input fields

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button, TextInput } from 'react-native';
+import { UNITS, LANGUAGE, CNT, APPID, BASE_URL  } from "@env"
+
 
 export default function App() {
+  const [cidade, setCidade] = useState('')
+  const [previsoes, setProvisoes] = useState([])
+  const capturarCidade = (cidadeDigitada) => {
+    setCidade(cidadeDigitada)
+  }
+
+
+
+
+  /* const endPoint = `https://api.openweathermap.org/data/2.5/forecast?lang=${lang}&units=${units}&cnt=${cnt}&appid=${appid}&q=` */
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.containerView}>
+      <View style={styles.entradaView}>
+        <TextInput style={styles.cidadeTextInput}
+          value={cidade}
+          onChangeText={capturarCidade}
+          placeholder="Digite o nome de uma cidade"/>
+        <Button title="ok"/>
+
+      </View>
+      <FlatList
+        data={previsoes}
+        renderItem={p=> (
+          <Text>{JSON.stringfy(p)}</Text>
+        )}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerView: {
+    padding: 40,
   },
+  entradaView: {
+    marginBottom: 8
+  },
+  cidadeTextInput: {
+    padding: 12,
+    borderBottomColor: '#FF9800',
+    borderBottomWidth: 2,
+    marginBottom: 4
+  }
+  
 });
